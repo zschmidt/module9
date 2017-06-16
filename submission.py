4bc9c7127c18234195a57dcdbedb4247960919b6
Zach Schmidt <zschmidt@users.noreply.github.com>
2017-06-15 21:18:10 -0700

    File added automatically at 2017-06-15 21:18:10

--- submission.py
+++ submission.py
@@ -1,25 +1,2 @@
-def predictor( row ):
-	m_mode = 'Yes'
-	h_mode = '1'
-	grad = row['Education']  # no empties
-	area = row['Property_Area']  # no empties
-	married = row['Married'] if row['Married'] != '' else m_mode
-	history = row['Credit_History'] if row['Credit_History'] != '' else h_mode
-		
-	if grad == 'Graduate':
-		if history == '1':
-			return 'Y'
-		else:
-			return 'N'
-	else:
-		if area != 'Rural':
-			if married == 'Yes':
-				return 'Y'
-			else:
-				return 'N'
-		else:
-			return 'N'
-
-	raise ValueError('No leaf reached for row: ' + row)
-	cases = [[predictor(row), row['Loan_Status']] for row in loan_table]
-
+def butts():
+    return 'test'
